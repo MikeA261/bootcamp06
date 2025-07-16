88555519c1b21662b16fc1222799856976f588d9
mikea261 <michael_261@hotmail.com>
2025-07-16 19:22:55 -0300

    Se corrigieron erroes del ejercico A

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var gatos = {gato1 :'😺', gato2 :'😸', gato3 :'😹'};
+
+var cantidadDeGatos = 10
+
+for(i= cantidadDeGatos; i > 0;){
+    if(i > 0){
+        console.log(gatos.gato1)
+        i--
+    }if(i > 0){
+            console.log(gatos.gato2)
+            i--
+        }if(i > 0){
+            console.log(gatos.gato3)
+            i--
+    }else break
+}
